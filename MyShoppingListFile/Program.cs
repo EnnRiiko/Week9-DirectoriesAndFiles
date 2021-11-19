7e72d1078318b5e92077701b9b9778c5da6258a1
Enn-Riiko Talivere <etaliver@tktk.ee>
2021-11-19 17:41:33 +0200

    create Directories and Files program

---
+++ MyShoppingListFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppingListFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopactive = true;
+
+            while (loopactive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter you wish:");
+                    string userWish = Console.ReadLine();
+                    myShoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopactive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine($"Your wish: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
